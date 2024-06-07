c89976c606f98638f15fe1da79d16d67877e0d35
Anna Zhukovskaya <kulnis.anna@gmail.com>
2024-06-07 15:31:52 +0300

    nb1

--- src/main/java/by/zhukovskaya/Main.java
+++ src/main/java/by/zhukovskaya/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello QA GURU!!!");
-        System.out.printf("Conflict 2");
+        System.out.printf("Conflict 2.1");
         System.out.printf("NO Conflict");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
